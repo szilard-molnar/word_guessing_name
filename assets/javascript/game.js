d8be0b2a4b9cf29d4c3f4d01a0a529b219e34940
szilard-molnar <mszilard86@gmail.com>
2019-06-30 17:28:20 -0400

    counting down is working, but not completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,19 @@
 // start a new game
 $(document).ready(function() {
 // create an array of words, theme is fruits
-var fruitArray = ["apple", "banana", "grapes", "orange", "lemon", "lime", "avocado", "almond", "pear", "strawberry", "raspberry", "cherry", "dragonfruit", "coconut", "mango"];
+var fruitArray = ["apple", "banana", "grapes", "orange", "lemon", "lime", "avocado", "almond", "pear", "strawberry", "raspberry", "cherry", "dragonfruit", "coconut", "mango", "grapefruit", "jackfruit", "chestnut"];
 
 var messageArray = ["You win!!!", "Sorry, you lost", "You guessed this letter aleady..."];
 var underscoreArray = [];
 var usedLetters = [];
 var letters = [];
-var remainingGuesses = 10;
+var remainingGuesses = 15;
 
 // get another random word by pressing "restart" button
     var randomFruit = fruitArray[Math.floor(Math.random() * fruitArray.length)];
     console.log(randomFruit);
 
-// set remaining guesses to 10
-   $("#remaining").append(remainingGuesses);
+// set remaining guesses to 15
 
 // split the random word into individual letters and put into wordSplit array
     var wordSplit = randomFruit.split('');
@@ -28,20 +27,27 @@ var remainingGuesses = 10;
     }
 
 // consolelogging the key that is being pressed 
-    document.addEventListener("keydown", function(event) 
+    document.addEventListener("keypress", function(event) 
     {
     console.log(event.key);
+    console.log(remainingGuesses);
+    remainingGuesses--;
+    console.log(remainingGuesses);
+    $("#remaining").html(remainingGuesses);
+
+    console.log(typeof event.key);
+    
+    if(typeof (guessLetter) === 'string')
+    {
+        alert("please type a letter");
+    }
 
 // putting input letter into lower case, and show letters in Guessed Letters group.
     var guessLetter = event.key.toLowerCase();  
-    letters.push(guessLetter);
-    document.getElementById("guessedLetters").innerHTML = letters.join("");
-    
-    /*if(typeof guessLetter === "number")
-        {
-            alert("Please type a letter!");
-        } */
 
+    letters.push(guessLetter);
+    document.getElementById("guessedLetters").innerHTML = letters.join(""); 
+    
 // splitting random words into separate letters
     for(var i = 0; i < randomFruit.length; i++)
     {
@@ -50,21 +56,35 @@ var remainingGuesses = 10;
             underscoreArray[i] = guessLetter;
             console.log(guessLetter, wordSplit[i]);
             document.getElementById("underscores").innerHTML = underscoreArray.join("");
+            
         }
-        if(guessLetter[i] !== wordSplit[i])
+    
+        var currentWordLine = document.getElementById("underscores").innerHTML;
+        if(currentWordLine.indexOf("_") === -1)
         {
-        remainingGuesses = remainingGuesses - 1;
+            var completeWord = true;
+
+            if(completeWord = true)
+            {
+                alert(messageArray[0]);
+            }
+        
+            else(remainingGuesses === 0)
+            {
+            alert(messageArray[1]);
+            }
         }
     }
 
-})
-    if(remainingGuesses = 0)
+
+    function restart ()
     {
-        alert(messageArray[1]);
+    var underscoreArray = [];
+    var usedLetters = [];
+    var letters = [];
+    var remainingGuesses = 15;
+    var randomFruit = fruitArray[Math.floor(Math.random() * fruitArray.length)];
+    console.log(randomFruit);
     }
-
-
-
-// make a var/array that stores the "tried" letters
-// word display
+})
 })
