d0746f7125887d32643a1c3c4d4bc72dc43be972
szilard-molnar <mszilard86@gmail.com>
2019-06-29 20:39:26 -0400

    set the remaining guesses to 10

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,21 +7,27 @@ var messageArray = ["You win!!!", "Sorry, you lost", "You guessed this letter al
 var underscoreArray = [];
 var usedLetters = [];
 var letters = [];
-var remainingGuesses = 15;
+var remainingGuesses = 10;
 
 // get another random word by pressing "restart" button
     var randomFruit = fruitArray[Math.floor(Math.random() * fruitArray.length)];
     console.log(randomFruit);
 
+// set remaining guesses to 15
+   $("#remaining").append(remainingGuesses);
+
+// split the random word into individual letters and put into wordSplit array
     var wordSplit = randomFruit.split('');
     console.log(wordSplit); 
 
+// the random word letters are being replaced by "_" 
     for(var i = 0; i < randomFruit.length; i++) 
     {
     underscoreArray[i] = " _ ";
     $("#underscores").append("<span>"+underscoreArray[i]+"</span>")
     }
 
+// 
     document.addEventListener("keydown", function(event) 
     {
     console.log(event.key);
@@ -30,15 +36,21 @@ var remainingGuesses = 15;
     letters.push(guessLetter);
     document.getElementById("guessedLetters").innerHTML = letters.join("");
     
-    document.getElementById("underscores").innerHTML = usedLetters.join('');
+    /*if(typeof guessLetter === "number")
+        {
+            alert("Please type a letter!");
+        } */
+
     for(var i = 0; i < randomFruit.length; i++)
     {
-        if(guessLetter == wordSplit[i]) 
+        if(guessLetter === wordSplit[i]) 
         {
             underscoreArray[i] = guessLetter;
+            console.log(guessLetter, wordSplit[i]);
             document.getElementById("underscores").innerHTML = underscoreArray.join("");
         }
     }
+
 })
 
 
