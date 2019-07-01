7fc51fda44bf319dfcec88beb1e75ff09c34a52d
szilard-molnar <mszilard86@gmail.com>
2019-07-01 01:02:20 -0400

    losing message/alert works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 // create an array of words, theme is fruits
 var fruitArray = ["apple", "banana", "grapes", "orange", "lemon", "lime", "avocado", "almond", "pear", "strawberry", "raspberry", "cherry", "dragonfruit", "coconut", "mango", "grapefruit", "jackfruit", "chestnut"];
 
-var messageArray = ["You win!!!", "Sorry, you lost", "You guessed this letter aleady..."];
+var messageArray = ["You win!!!", "Sorry, you lost", "You guessed this letter aleady...", "Please press a letter"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", ",", "/", "?", ";", "[", "]", "=", "`"];
 var underscoreArray = [];
-var usedLetters = [];
 var letters = [];
 var remainingGuesses = 15;
 
@@ -36,55 +36,61 @@ var remainingGuesses = 15;
     $("#remaining").html(remainingGuesses);
 
     console.log(typeof event.key);
-    
-    if(typeof (guessLetter) === 'string')
-    {
-        alert("please type a letter");
-    }
 
 // putting input letter into lower case, and show letters in Guessed Letters group.
-    var guessLetter = event.key.toLowerCase();  
+    var guessLetter = event.key.toLowerCase(); 
 
     letters.push(guessLetter);
+
+    for(var j = 0; j < numbers.length; j++)
+    {
+        if(event.key === numbers[j])
+        {
+            alert(messageArray[3]);
+        }
+    }
+
     document.getElementById("guessedLetters").innerHTML = letters.join(""); 
     
 // splitting random words into separate letters
     for(var i = 0; i < randomFruit.length; i++)
     {
-        if(guessLetter === wordSplit[i]) 
+        if(guessLetter === wordSplit[i])  
         {
             underscoreArray[i] = guessLetter;
             console.log(guessLetter, wordSplit[i]);
             document.getElementById("underscores").innerHTML = underscoreArray.join("");
-            
-        }
-    
-        var currentWordLine = document.getElementById("underscores").innerHTML;
-        if(currentWordLine.indexOf("_") === -1)
-        {
-            var completeWord = true;
-
-            if(completeWord = true)
-            {
-                alert(messageArray[0]);
-            }
-        
-            else(remainingGuesses === 0)
-            {
-            alert(messageArray[1]);
-            }
-        }
+        } 
     }
-
-
-    function restart ()
+    if(remainingGuesses === 0)
     {
-    var underscoreArray = [];
-    var usedLetters = [];
-    var letters = [];
-    var remainingGuesses = 15;
-    var randomFruit = fruitArray[Math.floor(Math.random() * fruitArray.length)];
-    console.log(randomFruit);
+        alert(messageArray[1]);
     }
-})
+    if(document.getElementById("underscores") === 0)
+    {
+        alert(messageArray[0]);
+    }
+    
+
+    function startOver()
+    {
+        var underscoreArray = [];
+        var usedLetters = [];
+        var letters = [];
+        var remainingGuesses = 15;
+        var randomFruit = fruitArray[Math.floor(Math.random() * fruitArray.length)];
+        var wordSplit = randomFruit.split(''); 
+        for(var i = 0; i < randomFruit.length; i++) 
+        {
+            underscoreArray[i] = " _ ";
+            $("#underscores").append("<span>"+underscoreArray[i]+"</span>");
+        }
+    }
+    
+    $("#restart-button").on("click", function()
+    {
+       
+    });
+
+    })
 })
