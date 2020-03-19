8fae75e56b66e316933dd4e6539f784a2962287d
szilard-molnar <mszilard86@gmail.com>
2020-03-19 15:53:27 -0400

    hangman draws when wrong guess happens

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,11 +12,23 @@ let completeWord;
 let randomFruit;
 let wordSplit;
 
+
+// initialize game
 function startGame() {
     underscoreArray = [];
     $( "#guessedLetters" ).empty();
     remainingGuesses = 10;
     completeWord = false;
+    $( "#standingTree" ).css("visibility", "hidden");
+    $("#standingTreeTop").css("visibility", "hidden");
+    $("#rope").css("visibility", "hidden");
+    $("#head").css("visibility", "hidden");
+    $("#body").css("visibility", "hidden");
+    $("#left-arm").css("visibility", "hidden");
+    $("#right-arm").css("visibility", "hidden");
+    $("#left-leg").css("visibility", "hidden");
+    $("#right-leg").css("visibility", "hidden");
+
 
 // random fruit from fruitArray
     randomFruit = fruitArray[Math.floor(Math.random() * fruitArray.length)];
@@ -31,6 +43,8 @@ function startGame() {
 strReplace();
 
 }
+
+
 // function to replace letters with "_"
 function strReplace(){
     for(let i = 0; i < wordSplit.length; i++)
@@ -41,9 +55,10 @@ function strReplace(){
     document.getElementById("underscores").innerHTML = underscoreArray;
 }
 
-// check if the input is in the word or not. if yes, reveal letter. if not, reduce remaining guesses by 1.
 
+// check if the input is in the word or not. if yes, reveal letter. if not, reduce remaining guesses by 1.
 function checkInput() {
+    drawMan();
     //console.log("this is the" + underscoreArray);
     if(remainingGuesses > 0)
     {  
@@ -78,6 +93,7 @@ function checkInput() {
     }
 }
 
+// function gets called when the player loses
 function ifYouLose() {
     if(remainingGuesses === 0)
     {
@@ -88,6 +104,7 @@ function ifYouLose() {
     }
 }
 
+// function gets called when the player wins
 function ifYouWin() {
     if(underscoreArray.indexOf("_") === -1) 
     {
@@ -98,8 +115,51 @@ function ifYouWin() {
     }
 }
 
+// function gets called when there is a missed guess and the hangman figure gets built up.
+function drawMan() {
+    if(remainingGuesses === 10)
+    {
+        $("#standingTree").css("visibility", "visible");
+    }
+    else if(remainingGuesses === 9)
+    {
+        $("#standingTreeTop").css("visibility", "visible");
+    }
+    else if(remainingGuesses === 8)
+    {
+        $("#rope").css("visibility", "visible");
+    }
+    else if(remainingGuesses === 7)
+    {
+        $("#head").css("visibility", "visible");
+    }
+    else if(remainingGuesses === 6)
+    {
+        $("#body").css("visibility", "visible");
+    }
+    else if(remainingGuesses === 5)
+    {
+        $("#left-arm").css("visibility", "visible");
+    }
+    else if(remainingGuesses === 4)
+    {
+        $("#right-arm").css("visibility", "visible");
+    }
+    else if(remainingGuesses === 3)
+    {
+        $("#left-leg").css("visibility", "visible");
+    }
+    else if(remainingGuesses === 2)
+    {
+        $("#right-leg").css("visibility", "visible");
+    }
+}
+
+// eventlistener that calls checkInput function when any key is pressed
 document.addEventListener("keydown", checkInput);
 
+
+// function that gets called when restart button gets called.
 $("#restart-button").click(function(){
     letters.length = 0;
     startGame();
